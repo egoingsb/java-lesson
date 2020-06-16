726b24ff70c1c42457e87205a71de2c6a1907e02
egoing <egoinglee@localhost>
2020-06-16 23:31:33 +0900

    상속구현

--- src/AccountAddInheritance.java
+++ src/AccountAddInheritance.java
@@ -1,20 +1,38 @@
 // AccountAddConstructor 의 내용을 수정하면 안되는 상황에서 동작방법을 수정하고 싶은 경우에 상속을 이용했습니다. 
 
-public class AccountAddInheritance extends AccountAddConstructor{
+class AccountingCompleteMessage extends Accounting2{
+
+	AccountingCompleteMessage(double total, Person2[] profitRate) {
+		super(total, profitRate);
+	}
+	
+	public String makeOutput() {
+		String output = "";
+		output += "매출 : "+total+"\n";
+		output += "부가가치세 : "+get_vat()+"\n";
+		output += "총이익 : "+get_income() +"\n";
+		
+		int i=0;
+		while(i<profitRate.length) {
+			output += profitRate[i].getInfo()+"의 이익 : " + profitRate[i].profitRate * get_income() + "\n";
+			i++; // i = i + 1과 같습니다. 
+		}
+		output += "complete!\n";
+		return output;
+	}
+	
+}
+
+public class AccountAddInheritance {
 	public static void main(String[] args) {
 		
 		Person2 p1 = new Person2(1, "Michael", 0.5);
 		Person2 p2 = new Person2(2, "Calvin", 0.3);
 		Person2 p3 = new Person2(3, "Robin", 0.2);
-		 
 		Person2[] profitRate = {p1, p2, p3};
-		double total = Double.parseDouble(args[0]);  
-		double VATrate = get_vat_rate(total);
-		double VAT = get_vat(total, VATrate);
-		double income = get_income(total, VAT);
-		String output = makeOutput(profitRate, total, VAT, income);
+		Accounting2 acc = new AccountingCompleteMessage(Double.parseDouble(args[0]), profitRate);
+		String output = acc.makeOutput();
 		Print.screen(output);
 		Print.file(output);
-		System.out.println("작업이 성공적으로 끝났습니다.");
 	}
 }
