ee07025da099ee317752d3f48be536cfec5796b2
egoing <egoinglee@localhost>
2020-06-16 22:47:08 +0900

    Account 인스턴스화

--- src/AccountAddInstance.java
+++ src/AccountAddInstance.java
@@ -6,6 +6,46 @@ class Person{
 		return "id:"+id+",name:,profit";
 	}
 }
+class Accounting{
+	public double total;
+	public Person[] profitRate;
+	
+	public double get_vat_rate() {
+		double VATrate;
+		if(total > 10000) {
+			VATrate = 0.1;
+		} else {
+			VATrate = 0;
+		}
+		return VATrate;
+	}
+
+	
+
+	public String makeOutput() {
+		String output = "";
+		output += "매출 : "+total+"\n";
+		output += "부가가치세 : "+get_vat()+"\n";
+		output += "총이익 : "+get_income() +"\n";
+		
+		int i=0;
+		while(i<profitRate.length) {
+			output += profitRate[i].getInfo()+"의 이익 : " + profitRate[i].profitRate * get_income() + "\n";
+			i++; // i = i + 1과 같습니다. 
+		}
+		
+		output += "\n";
+		return output;
+	}
+
+	public double get_income() {
+		return total - get_vat();
+	}
+
+	public double get_vat() {
+		return total*get_vat_rate();
+	}
+}
 
 public class AccountAddInstance {
 
@@ -27,50 +67,14 @@ public class AccountAddInstance {
 		p3.profitRate = 0.2;
 		
 		Person[] profitRate = {p1, p2, p3};
-		double total = Double.parseDouble(args[0]);  
-		double VATrate = get_vat_rate(total);
-		double VAT = get_vat(total, VATrate);
-		double income = get_income(total, VAT);
-		String output = makeOutput(profitRate, total, VAT, income);
+		Accounting acc = new Accounting();
+		acc.total = Double.parseDouble(args[0]);
+		acc.profitRate = profitRate;
+		String output = acc.makeOutput();
 		Print.screen(output);
 		Print.file(output);
-
-	}
-
-	public static double get_vat_rate(double total) {
-		double VATrate;
-		if(total > 10000) {
-			VATrate = 0.1;
-		} else {
-			VATrate = 0;
-		}
-		return VATrate;
 	}
 
 	
-
-	public static String makeOutput(Person[] profitRate, double total, double VAT, double income) {
-		String output = "";
-		output += "매출 : "+total+"\n";
-		output += "부가가치세 : "+VAT+"\n";
-		output += "총이익 : "+income +"\n";
-		
-		int i=0;
-		while(i<profitRate.length) {
-			output += profitRate[i].getInfo()+"의 이익 : " + profitRate[i].profitRate * income + "\n";
-			i++; // i = i + 1과 같습니다. 
-		}
-		
-		output += "\n";
-		return output;
-	}
-
-	public static double get_income(double total, double VAT) {
-		return total - VAT;
-	}
-
-	public static double get_vat(double total, double VATrate) {
-		return total*VATrate;
-	}
 
 }
