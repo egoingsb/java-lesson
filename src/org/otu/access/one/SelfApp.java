7ad435cda9a9b93573bb08c490a58f0694e84692
egoing <egoinglee@localhost>
2020-06-17 06:38:34 +0900

    상

--- src/org/otu/access/one/SelfApp.java
+++ src/org/otu/access/one/SelfApp.java
@@ -1,15 +1,35 @@
 package org.otu.access.one;
 
+class SelfAppInheritance extends org.otu.access.two.PublicApp{
+	public static void main(String[] args) {
+		SelfAppInheritance self = new SelfAppInheritance();
+		System.out.println(self.publicVal); // ok
+//		System.out.println(self.defaultVal); // fail
+		System.out.println(self.protectedVal); // ok
+//		System.out.println(self.privateVal); // fail
+	}
+}
 
-public class LoaderApp {
+public class SelfApp {
+	
+	public String publicVal = "public";
+	private String privateVal = "private";
+	String defaultVal = "default";
+	protected String protectedVal = "protected";
 
 	public static void main(String[] args) {
 		
+		SelfApp self = new SelfApp();
 		PublicApp publicIn = new PublicApp();
 		DefaultApp defaultIn = new DefaultApp();
 		org.otu.access.two.PublicApp publicOut = new org.otu.access.two.PublicApp();
 		//default인 경우 다른 패키지에서 접근이 되지 않습니다. 
 		//org.otu.excise.two.DefaultApp defaultOut = new org.otu.excise.two.DefaultApp();
+		
+		System.out.println(self.publicVal); // ok
+		System.out.println(self.defaultVal); // ok
+		System.out.println(self.protectedVal); // ok
+		System.out.println(self.privateVal); // ok
 		
 		System.out.println(publicIn.publicVal); // ok
 		System.out.println(publicIn.defaultVal); // ok
@@ -25,8 +45,6 @@ public class LoaderApp {
 		//System.out.println(publicOut.defaultVal); // fail
 		//System.out.println(publicOut.protectedVal); // fail
 		// System.out.println(publicOut.privateVal); // fail
-		
-		
 		
 	}
 
