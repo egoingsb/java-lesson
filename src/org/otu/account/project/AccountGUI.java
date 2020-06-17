d3e52ccb2774ccd12ba8b629104f5371201e06ab
egoing <egoinglee@localhost>
2020-06-17 21:22:17 +0900

    GUI 연습장 추가

---
+++ src/org/otu/account/project/AccountGUI.java
@@ -0,0 +1,75 @@
+package org.otu.account.project;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+
+public class AccountGUI {	
+	public static void main(String[] args) throws FileNotFoundException {
+	        
+			String data = get_data();
+		
+		 	JFrame fr = new JFrame("회계");
+	        JPanel inputPanel = new JPanel();
+	        inputPanel.setLayout(new  BoxLayout(inputPanel, BoxLayout.X_AXIS));
+	        JTextField money = new JTextField(20);
+	        inputPanel.add(money);
+	        
+	        JButton submit = new JButton("입력");
+	        inputPanel.add(submit);
+	        
+	        JPanel outputPanel = new JPanel();
+	        JTextArea result = new JTextArea(12, 25); 
+	        result.setText(data);
+	        JScrollPane scrollPane = new JScrollPane(result);
+	        outputPanel.add(scrollPane);
+	        JPanel rootPanel = new JPanel();
+	        rootPanel.add(inputPanel);
+	        rootPanel.add(outputPanel);
+	        
+	        submit.addActionListener(new ActionListener(){ //익명클래스로 리스너 작성
+				public void actionPerformed(ActionEvent e){
+					Person p1 = new Person(1, "Michael", 0.5);
+					Person p2 = new Person(2, "Calvin", 0.3);
+					Person p3 = new Person(3, "Robin", 0.2);
+					Person[] profitRate = {p1, p2, p3};
+					Accounting acc = new Accounting(Double.parseDouble(money.getText()), profitRate);
+					String output = acc.makeOutput();
+					Print.file(output);
+					try {
+						result.setText(get_data());
+					} catch (FileNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			});
+	        
+	        fr.setContentPane(rootPanel);
+	        fr.setSize(360, 300);
+	        fr.setVisible(true);
+	    }
+
+	public static String get_data() throws FileNotFoundException {
+		String data = "";
+		for(Scanner sc = new Scanner(new File("journal.txt")); sc.hasNext(); ) {
+		  String line = sc.nextLine();
+		  data+=line+"\n";
+		}
+		return data;
+	}
+
+}
---
+++ src/org/otu/account/project/AccountGUIPlayground.java
@@ -0,0 +1,28 @@
+package org.otu.account.project;
+
+public class AccountGUIPlayground {	
+	public static void main(String[] args) {
+	        
+			// 회계정보를 담고 있는 파일에서 데이터를 읽어옵니다. 
+			// 검색 : file read in java  예) https://stackoverflow.com/a/4909018
+			
+			
+			// 스윙로 UI를 구성합니다. 
+			// 검색 : java swing sample   예) https://www.javatpoint.com/java-swing
+		
+			// 여러 줄의 텍스트를 표시할 콘트롤 (textarea), 금액을 입력할 한줄 텍스트 입력 콘트롤 (textfield), 버튼을 UI에 붙입니다. 
+			// 검색 : textarea swing, textfield swing, button swing 
+		
+			// 버튼을 눌렀을 때 아래 코드가 실행되서 출력할 결과를 얻어내세요. 
+			// 검색 : swing button addActionListener 예) https://stackoverflow.com/a/284934			
+			//Person p1 = new Person(1, "Michael", 0.5);
+			//Person p2 = new Person(2, "Calvin", 0.3);
+			//Person p3 = new Person(3, "Robin", 0.2);
+			//Person[] profitRate = {p1, p2, p3};
+			//Accounting acc = new Accounting(Double.parseDouble(money.getText()), profitRate);
+			//String output = acc.makeOutput();
+			//Print.file(output);
+			
+			// 저장된 파일을 textarea에 표시해주세요. 
+	}
+}
